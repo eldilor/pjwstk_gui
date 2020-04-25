1100248c43f3826c9bd01f9a1dcb81e00d40babc
Oskar Kupski <oskar.kupski@gmail.com>
2020-04-25 09:59:43 +0200

    Project, Product class added

---
+++ src/p1/Entity/Product.java
@@ -0,0 +1,31 @@
+package p1.Entity;
+
+import p1.utils.PriceFormatter;
+
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+
+    public Product(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String toString() {
+        return name + " - " + PriceFormatter.format(price);
+    }
+}
